d33c9fe44a4222b8a9c29463bf37caee78a3c6f2
6610110554 <6610110554@email.psu.ac.th>
2024-02-02 18:36:03 +0700

    excel

--- client/src/EntryPageforstaff.js
+++ client/src/EntryPageforstaff.js
@@ -1,26 +1,37 @@
-import './App.css';
-import React, { useState, useEffect,useRef } from 'react';
-import { Spin, Divider, Typography, Modal, Form, Input, Button, DatePicker } from 'antd';
-import moment from 'moment';
-import axios from 'axios';
-import Entryforstaff from './components/entryforstaff';
-import PostEntry from './components/PostEntry';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import { useLocation } from 'react-router-dom';
-import { useSessionStorage } from './SessionStorage/useSessionStorage';
+import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Spin,
+  Divider,
+  Typography,
+  Modal,
+  Form,
+  Input,
+  Button,
+  DatePicker,
+} from "antd";
+import moment from "moment";
+import axios from "axios";
+import Entryforstaff from "./components/entryforstaff";
+import PostEntry from "./PostEntry";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import { useSessionStorage } from "./SessionStorage/useSessionStorage";
 import * as xlsx from "xlsx";
 
-
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:1337";
-const URL_TXACTIONS = 'api/events/:id/entries?filters[id][$eq]';
-const URL_ENTRY = 'api/entries';
-const URL_ENTRY1 = 'api/entries?populate=*'
-
+axios.defaults.baseURL =
+  process.env.REACT_APP_BASE_URL || "http://localhost:1337";
+const URL_TXACTIONS = "api/events/:id/entries?filters[id][$eq]";
+const URL_ENTRY = "api/entries";
+const URL_POSTENTRY = "api/users?populate=role&filters[role][name]=Student";
 
 const EntryPageforstaff = () => {
+  const [excelData, setExcelData] = useState(null);
+  const [stdid, setStdid] = useState([]);
+  const [postSuccess, setPostSuccess] = useState(false);
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const itemId = searchParams.get('itemId');
+  const itemId = searchParams.get("itemId");
   const [isLoading, setIsLoading] = useState(false);
   const [transactionData, setTransactionData] = useState([]);
   const [editFormData, setEditFormData] = useState({});
@@ -29,49 +40,46 @@ const EntryPageforstaff = () => {
   const { handleLogout } = useSessionStorage();
   const { getItem } = useSessionStorage();
 
- 
-
   const fetchItems = async () => {
     try {
       setIsLoading(true);
-      const jwtToken = getItem('jwt');
-      console.log("MAaasada11111mma",jwtToken);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
+      const jwtToken = getItem("jwt");
+      console.log("MAaasada11111mma", jwtToken);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
       const response = await axios.get(`${URL_TXACTIONS}=${itemId}`);
-      
-      setTransactionData(response.data.data.flatMap(d => {
-        return d.attributes.entries.data.map(entry => ({
-          id: entry.id,
-          username: entry.attributes.owner.data.attributes.username,
-          result: entry.attributes.result,
-          
-          seen_datetime: entry.attributes.seen_datetime,
-          ack_datetime: entry.attributes.ack_datetime,
-          ConfirmView: entry.attributes.ConfirmView ? 'รับทราบแล้ว' : 'ยังไม่รับทราบ'
 
-          
-        }));
-      }));
-    
+      setTransactionData(
+        response.data.data.flatMap((d) => {
+          return d.attributes.entries.data.map((entry) => ({
+            id: entry.id,
+            username: entry.attributes.owner.data.attributes.username,
+            result: entry.attributes.result,
+
+            seen_datetime: entry.attributes.seen_datetime,
+            ack_datetime: entry.attributes.ack_datetime,
+            ConfirmView: entry.attributes.ConfirmView
+              ? "รับทราบแล้ว"
+              : "ยังไม่รับทราบ",
+          }));
+        })
+      );
     } catch (err) {
       console.log(err);
     } finally {
       setIsLoading(false);
     }
-  }
-
-  
+  };
 
   const addItem = async (itemId) => {
-    console.log("อะไรออกมาบ้าง",itemId);
+    console.log("อะไรออกมาบ้าง", itemId);
     try {
       setIsLoading(true);
-      const params = { ...itemId};
+      const params = { ...itemId };
       const response = await axios.post(URL_ENTRY, { data: params });
-      const { id,attributes } = response.data.data;
+      const { id, attributes } = response.data.data;
       setTransactionData([
         ...transactionData,
-        { id: id, key: id ,...attributes },
+        { id: id, key: id, ...attributes },
       ]);
     } catch (err) {
       console.log(err);
@@ -81,8 +89,6 @@ const EntryPageforstaff = () => {
       refreshData();
     }
   };
-  
-  
 
   const editItem = (itemId) => {
     const currentItem = transactionData.find((item) => item.id === itemId);
@@ -96,32 +102,40 @@ const EntryPageforstaff = () => {
       htmlType: "submit",
       content: (
         <Form
-        initialValues={currentItem}
-        onFinish={(values) => handleEdit(values, itemId)}
-      >
-        <Form.Item label="Student Name" name="username" rules={[{ required: true, message: 'Please enter a subject name!' }]}>
-        <Input disabled />
-        </Form.Item>
-        
-        <Form.Item label="Result" name="result" rules={[{ required: true, message: 'Please enter a Date-Time!' }]}>
-          <Input />
-        </Form.Item>
-       
-       
-        <Form.Item >
-          <Button type="primary" htmlType="submit">
-            Save
-          </Button>
-        </Form.Item>
-      </Form>
+          initialValues={currentItem}
+          onFinish={(values) => handleEdit(values, itemId)}
+        >
+          <Form.Item
+            label="Student Name"
+            name="username"
+            rules={[
+              { required: true, message: "Please enter a subject name!" },
+            ]}
+          >
+            <Input disabled />
+          </Form.Item>
+
+          <Form.Item
+            label="Result"
+            name="result"
+            rules={[{ required: true, message: "Please enter a Date-Time!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Save
+            </Button>
+          </Form.Item>
+        </Form>
       ),
       okButtonProps: {
-        style: { display: 'none' },
+        style: { display: "none" },
       },
       onCancel: () => {
         setEditFormData({});
         setIsEditing(false);
-      
       },
     });
   };
@@ -129,14 +143,14 @@ const EntryPageforstaff = () => {
   const handleEdit = async (values, itemId) => {
     try {
       setIsLoading(true);
-  
+
       const payload = {
         data: {
           ...values,
           id: itemId,
         },
       };
-  
+
       await axios.put(`${URL_ENTRY}/${itemId}`, payload);
       fetchItems();
     } catch (err) {
@@ -168,38 +182,79 @@ const EntryPageforstaff = () => {
     });
   };
 
+  const refreshData = () => {
+    const jwtToken = getItem("jwt");
+    console.log("MAamma", jwtToken);
+    axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
+  };
+
   const readUploadFile = (e) => {
     e.preventDefault();
     if (e.target.files) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const data = e.target.result;
-            const workbook = xlsx.read(data, { type: "array" });
-            const sheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[sheetName];
-            const json = xlsx.utils.sheet_to_json(worksheet);
-            console.log(json);
-        };
-        reader.readAsArrayBuffer(e.target.files[0]);
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
+        console.log(json);
+        setExcelData(json);
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
     }
-}
+  };
 
-  const refreshData = () => {
-    const jwtToken = getItem('jwt');
-      console.log("MAamma",jwtToken);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${jwtToken}`;
-    };
-  
+  const PostExcel = async () => {
+    try {
+      const jwtToken = getItem("jwt");
+      console.log("88888888888mma", jwtToken);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
+      const CheckUser = await axios.get(URL_POSTENTRY);
+      const filterUser = CheckUser.data.filter((item) =>
+        item.username.match(/^.*/)
+      );
+      const filterid = filterUser.map((item) => ({
+        id: item.id,
+        username: item.username,
+      }));
+      setStdid(filterid);
+      console.log(filterUser)
+      for (const excelRow of excelData) {
+        const studentId = excelRow[0];
+        const matchUser = filterUser.find(
+          (item) => item.username === studentId
+        );
+        console.log(matchUser)
+        if (matchUser) {
+          const userId = matchUser.id;
+          const response = await axios.post(URL_ENTRY, {
+            data: {
+              result: `${excelRow[1]}`,
+              owner: parseInt(userId),
+              event: parseInt(itemId),
+            },
+          });
+
+          console.log("Post Excel to Strapi success:", response.data);
+          setPostSuccess(true);
+          
+        }
+      }
+      fetchItems();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   useEffect(() => {
     refreshData();
     fetchItems();
-   
   }, []);
 
   return (
     <div className="App">
-        <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Brand>Staff Page</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -213,32 +268,40 @@ const EntryPageforstaff = () => {
         </Container>
       </Navbar>
       <header className="App-header">
-        <Spin spinning={isLoading}>
           <Typography.Title></Typography.Title>
-          
 
           <Divider>
             <h4>Subject entry</h4>
           </Divider>
-          <PostEntry onItemcreate={addItem}/>
-          <div style={{ marginTop: '10px' }}></div>
-          <form>
-    <label htmlFor="upload">Upload File</label>
-    <input
-        type="file"
-        name="upload"
-        id="upload"
-        onChange={readUploadFile}
-    />
-</form>
+          <PostEntry onItemcreate={addItem} />
+          <div style={{ marginTop: "10px" }}></div>
+          <div>
+      <form>
+        <label htmlFor="upload">Upload File</label>
+        <input
+          type="file"
+          name="upload"
+          id="upload"
+          onChange={readUploadFile}
+        />
+      </form>
+      
+        <Button
+          variant="success"
+          onClick={PostExcel}
+          type="primary" 
+        >
+          Upload
+        </Button>
+      
+    </div>
 
-<div style={{ marginTop: '20px' }}></div>
+          <div style={{ marginTop: "20px" }}></div>
           <Entryforstaff
             data={transactionData}
             onTransactionDeleted={deleteItem}
             onTransactionEdit={editItem}
           />
-        </Spin>
       </header>
     </div>
   );
--- client/src/PostEntry.js
+++ client/src/PostEntry.js
@@ -1,12 +1,24 @@
 import { Button, Form, Input } from 'antd';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import  axios from 'axios';
+import { useSessionStorage } from "./SessionStorage/useSessionStorage";
+
+
+axios.defaults.baseURL =
+  process.env.REACT_APP_BASE_URL || "http://localhost:1337";
+const URL_TXACTIONS = "api/events/:id/entries?filters[id][$eq]";
+const URL_ENTRY = "api/entries";
+const URL_POSTENTRY = "api/users?populate=role&filters[role][name]=Student";
 
 export default function PostEntry(props) {
   const [form] = Form.useForm();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const itemId = searchParams.get('itemId');
+  const { getItem } = useSessionStorage();
+
+  
 
   
   const initialValues = {
@@ -15,14 +27,35 @@ export default function PostEntry(props) {
     event: itemId, 
   };
 
+  const onFinish = async (data) => {
+    
+    const jwtToken = getItem("jwt");
+    axios.defaults.headers.common["Authorization"] = `Bearer ${jwtToken}`;
+    const CheckUser = await axios.get(URL_POSTENTRY);
+    const matchingUser = CheckUser.data.find(item => item.username === data.owner);
+
+    
+    if (matchingUser) {
+      const updatedData = {
+        ...data,
+        owner: matchingUser.id, 
+      };
+
+     
+      props.onItemcreate(updatedData);
+      form.resetFields();
+    } else {
+      console.log("User not found");
+      
+    }
+  };
+
+
   return (
     <Form
       form={form}
       layout="inline"
-      onFinish={(data) => {
-        props.onItemcreate(data);
-        form.resetFields();
-      }}
+      onFinish={onFinish}
       initialValues={initialValues} 
     >
       <Form.Item name="owner" label="Student Name" rules={[{ required: true }]}>
